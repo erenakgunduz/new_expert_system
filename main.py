2638911bdee11778ab3aed6123781158418bf543
erenakgunduz <coderenius@gmail.com>
2023-06-23 23:45:16 +0300

    Update main.py
    why not

--- main.py
+++ main.py
@@ -6,11 +6,26 @@ app = Microdot()
 Response.default_content_type = "text/html"
 
 css_path = "node_modules/@picocss/pico/css/pico.min.css"
+title = "New Expert System"
+
+head_tags = f"""
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href={css_path} />
+    <title>{title}</title>
+</head>
+"""
 
 
 @app.route(css_path)
 def pico_css(request):
     return Response.send_file(css_path)
+
+
+@app.route(f"{css_path}.map")
+def pico_css_map(request):
+    return Response.send_file(f"{css_path}.map")
 
 
 @app.route("/")
@@ -19,12 +34,7 @@ def hello(request):
     page_html = f"""
     <!DOCTYPE html>
     <html lang="en">
-        <head>
-            <meta charset="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <link rel="stylesheet" href={css_path} />
-            <title>Document</title>
-        </head>
+        {head_tags}
         <body>
             <main class="container">
                 <h1>{h1_text}</h1>
